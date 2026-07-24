f0394715324167d6759696d18de82bf0a39b6499
Srinivasvasam45 <srinivasvasam15@gmail.com>
2026-07-24 12:16:41 +0530

    skills error fixed

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -22,7 +22,7 @@
         "Power BI",
     ]
 
-    const Backend = [
+    const backend = [
         "Node.js",
         "Express>js",
         "Mongodb",
@@ -78,7 +78,7 @@
                 <div className='rounded-xl p-6 hover:-translate-y-1 transition-all'>
                     <h3 className="text-xl font-bold mb-4">Backend</h3>
                     <div className='flex flex-wrap gap-2'>
-                       {Backend.map((Backend,key) => (
+                       {backend.map((Backend,key) => (
                         <span key={Backend} className='bg-blue-500/10 text-blue-500 px-3 py-1 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130,246,0.2)] transition'>
                             {Backend}
                         </span>
